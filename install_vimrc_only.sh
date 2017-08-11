e2145dfc4076c994f950cc01f4962f59cfa9c323
Acaki <axsz4298@gmail.com>
2017-08-11 13:22:51 +0000

    Fix script

--- install_vimrc_only.sh
+++ install_vimrc_only.sh
@@ -3,12 +3,15 @@
 echo "Fetching newest version of this repository..."
 git pull origin master
 
-if [ -f $HOME/.vimrc ] && ! [ -f $HOME/.vimrc.old ]; then
+if [ -f $HOME/.vimrc ] &&  [ ! -f $HOME/.vimrc.old ]; then
   printf "Rename old vimrc file..."
   mv $HOME/.vimrc $HOME/.vimrc.old
   printf "ok\n"
 fi
 
-echo "Symlink .vimrc to home directory..."
-ln -s $PWD/.vimrc $HOME/.vimrc
+if [ ! -h $HOME/.vimrc ]; then
+  echo "Symlink .vimrc to home directory..."
+  ln -s $PWD/.vimrc $HOME/.vimrc
+fi
+
 echo "Installation complete."
