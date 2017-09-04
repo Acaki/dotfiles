d58db8795e5e23c0b2e5445180855cc68ec22186
Acaki <axsz4298@gmail.com>
2017-09-04 22:03:38 +0800

    Update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,29 +6,30 @@ olddir=$HOME/dotfiles_old
 files=".vimrc .gitconfig .screenrc .tmux.conf"
 
 echo "Fetching newest version of this repository..."
-git pull origin master
+git pull
 
-if [ ! -d $olddir ]; then
-	printf "Creating backup directory..."
-	mkdir $olddir
-	printf "ok\n"
+read -p "Do you want to backup existing dotfiles? [y/n] " backup
+if [[ $backup =~ ^[Yy]$ ]] && [ ! -d $olddir ]; then
+  printf "Creating backup directory..."
+  mkdir $olddir
+  printf "ok\n"
 fi
 
 for file in $files; do
-  read -p "Do you wish to install $file? [y/n] " yn
-  if [[ $yn =~ ^[Yy]$ ]]; then
-    if [ -f $HOME/$file ] && [ ! -L $HOME/$file ]; then
+  read -p "Do you want to install $file? [y/n] " install 
+  if [[ $install =~ ^[Yy]$ ]]; then
+    if [ -f $HOME/$file ] && [ ! -L $HOME/$file ] && [[ $backup =~ ^[Yy]$ ]]; then
       printf "Moving $HOME/$file to $olddir..."
       mv -n $HOME/$file $olddir/$file
       printf "ok\n"
     fi
     if [ ! -h $HOME/$file ]; then
       echo "Symlink $file to home directory..."
-      ln -s $PWD/$file $HOME/$file
+      ln -sf $PWD/$file $HOME/$file
       if [ $file == ".gitconfig" ]; then
-        read -p "Enter your name to be used in git: " name
+        read -p "*** Enter your name to be used in git ***: " name
         git config --global user.name "$name"
-        read -p "Enter your email to be used in git: " email
+        read -p "*** Enter your email to be used in git ***: " email
         git config --global user.email "$email"
       fi
     fi
