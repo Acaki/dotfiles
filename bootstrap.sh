5445fa83357c9321d9d66c8760fbb9d1c1f3884d
Acaki <axsz4298@gmail.com>
2017-09-07 14:21:42 +0800

    Update script to ignore git user settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,10 +27,20 @@ for file in $files; do
       echo "Symlink $file to home directory..."
       ln -sf $PWD/$file $HOME/$file
       if [ $file == ".gitconfig" ]; then
-        read -p "*** Enter your name to be used in git ***: " name
-        git config --global user.name "$name"
-        read -p "*** Enter your email to be used in git ***: " email
-        git config --global user.email "$email"
+        while read -p "*** Enter your name to be used in git ***: " name
+        do
+          if [ -n "$name" ]; then
+            printf "[user]\n\tname = %s" $name >> $HOME/.gitconfig.user
+            break;
+          fi
+        done
+        while read -p "*** Enter your email to be used in git ***: " email
+        do
+          if [ -n "$email" ]; then
+            printf "\n\temail = %s" $email >> $HOME/.gitconfig.user
+            break;
+          fi
+        done
       fi
     fi
   fi
