91840f62b0cef5362db57628042dc8f0d1586b6c
Acaki <axsz4298@gmail.com>
2018-08-07 13:53:36 +0800

    Fix script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 olddir=$HOME/dotfiles_old
-files=".vimrc .gitconfig .tmux.conf config/fish/config.fish"
+files=".vimrc .gitconfig .tmux.conf .config/fish/config.fish"
 
 echo "Fetching newest version of this repository..."
 git pull origin master
