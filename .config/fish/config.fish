a6a217e3a1f15a1d62e080901e44ed99a3ee57c7
Acaki <axsz4298@gmail.cm>
2018-11-07 13:47:25 +0800

    Update fish config to auto launch tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
-if test -d $HOME/.local/bin
-  set PATH $HOME/.local/bin $PATH
-end
+test -d $HOME/.local/bin; and set PATH $HOME/.local/bin $PATH
 set LANG en_US.UTF-8
 set LC_ALL en_US.UTF-8
+
+if test -z "$TMUX"; and test -n "$USE_TMUX"
+  tmux a 2>/dev/null; or cd; and exec tmux
+end
