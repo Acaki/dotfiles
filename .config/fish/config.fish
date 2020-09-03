c86a3ee9a80c8071f82b66f4fa4cf2c1321b2fc3
mason <mason.chen@cinnamon.is>
2020-09-03 10:16:19 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,42 @@ test -d $HOME/.local/bin; and set PATH $HOME/.local/bin $PATH
 set LANG en_US.UTF-8
 set LC_ALL en_US.UTF-8
 
-if test -z "$TMUX"; and begin test -n "$USE_TMUX"; or test -n "$SSH_CLIENT"; end
-   tmux a 2>/dev/null; or cd; and exec tmux
+fish_vi_key_bindings
+
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent                                                                                                                                                                    
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV 
+    . $SSH_ENV > /dev/null
+    ssh-add
 end
 
-fish_vi_key_bindings
+function test_identities                                                                                                                                                                
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ] 
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end  
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end  
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else 
+        start_agent
+    end  
+end
