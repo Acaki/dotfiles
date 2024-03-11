5fddd95ddcacd408dd8af3227cedaa608eafe77f
Acaki <github-20201224@wdy.one>
2024-03-11 20:30:08 +0800

    fix: home and end keys in insert mode on macos

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,8 @@
 function fish_user_key_bindings
   fzf_key_bindings
   fish_vi_key_bindings
+  bind \cA beginning-of-line
+  bind \cE end-of-line
+  bind --mode insert \cA beginning-of-line
+  bind --mode insert \cE end-of-line
 end
